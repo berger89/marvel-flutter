8bc069d2b3d86fad5ece12c01aa52be0929cf247
berger <berger.alexander89@gmail.com>
2020-11-01 20:49:14 +0100

    Added splashscreen

--- android/app/src/main/kotlin/com/example/marvel_flutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/marvel_flutter/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.marvel_flutter
 
-import androidx.annotation.NonNull;
+import android.os.Bundle
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Thread.sleep(1500)
+    }
+
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine);
     }
